18b009be5e71b8c046b85647d36a1bc324445e7b
sbuteryd <sbuteryd@qq.com>
2019-09-26 14:18:13 +0800

    init

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@ import React,{useState} from 'react';
 import './App.css';
 
 function App (){
-    const [theme,setThem] = useState('light')
-    const toDark = () => setThem('dark')
-    const toLight = () => setThem('light')
-    console.log(theme)
     return (
-        <div className={theme}>
-            {theme === "light" ?
-                <button onClick={toDark}>🔦</button>
-                :<button onClick={toLight}>💡</button>}
+        <div>
+            hello world
         </div>
     )
-
 }
 export default App;
