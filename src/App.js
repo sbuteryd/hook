96360f38ba73e36ec7079c45f6c78931227d7a6f
sbuteryd <sbuteryd@qq.com>
2019-09-26 14:16:15 +0800

    light to dark

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App (){
+    const [theme,setThem] = useState('light')
+    const toDark = () => setThem('dark')
+    const toLight = () => setThem('light')
+    console.log(theme)
+    return (
+        <div className={theme}>
+            {theme === "light" ?
+                <button onClick={toDark}>🔦</button>
+                :<button onClick={toLight}>💡</button>}
+        </div>
+    )
 
+}
 export default App;
