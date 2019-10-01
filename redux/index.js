2d34e98a56871b312224a7f5d0c0e2dc636f8974
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-02 07:06:11 +0800

    listen store

--- redux/index.js
+++ redux/index.js
@@ -5,8 +5,27 @@ function createStore() {
 //    3、get state
 //    4、update state
     let state
+    let listeners =[]
     const getState = ()=> state
+    const subscribe = (listener)=>{
+        listeners.push(listener)
+        return ()=>{
+            listeners.filter((l)=> l !==listener)
+        }
+    }
     return {
-        getState
+        getState,
+        subscribe
     }
 }
+
+const store = createStore()
+store.subscribe(()=>{
+    console.log('The new State is',store.getState())
+})
+
+const unsubscribe = store.subscribe(()=>{
+    console.log('The store changed.')
+})
+
+unsubscribe()
